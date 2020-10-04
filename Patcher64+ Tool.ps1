1fdba2daf3b47a48e70de4bcb1a0bccc48b43226
Robin <8747034+Admentus64@users.noreply.github.com>
2020-10-04 18:35:29 +0200

    Update Patcher64+ Tool.ps1

--- Patcher64+ Tool.ps1
+++ Patcher64+ Tool.ps1
@@ -15,8 +15,8 @@ Add-Type -AssemblyName 'System.Drawing'
 # Setup global variables
 
 $global:ScriptName = "Patcher64+ Tool"
-$global:VersionDate = "02-10-2020"
-$global:Version     = "v7.6.1"
+$global:VersionDate = "04-10-2020"
+$global:Version     = "v7.7.0"
 
 $global:GameType = $global:GamePatch = $global:CheckHashSum = ""
 $global:GameFiles = $global:Settings = @{}
