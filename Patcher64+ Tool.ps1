97da536d47772838503c4f24abd50deef4b6e7d0
Admentus64 <8747034+Admentus64@users.noreply.github.com>
2021-07-25 10:23:49 +0200

    Update v14.2.2

--- Patcher64+ Tool.ps1
+++ Patcher64+ Tool.ps1
@@ -24,8 +24,8 @@ Add-Type -AssemblyName 'System.Drawing'
 # Setup global variables
 
 $global:ScriptName = "Patcher64+ Tool"
-$global:VersionDate = "2021-07-15"
-$global:Version     = "v14.2.1"
+$global:VersionDate = "2021-07-25"
+$global:Version     = "v14.2.2"
 $global:SystemDate  = Get-Date -Format yyyy-MM-dd-HH-mm-ss
 
 $global:CommandType = $MyInvocation.MyCommand.CommandType.ToString()
