02995d015f74dc8f333b616649cbd87042a09bbe
Robin <8747034+Admentus64@users.noreply.github.com>
2022-05-09 15:45:49 +0200

    Update v17.3.5

--- Files/Scripts/Options/Mario Kart 64.psm1
+++ Files/Scripts/Options/Mario Kart 64.psm1
@@ -10,19 +10,20 @@ function PatchOptions() {
 function ByteOptions() {
     
     if (IsChecked $Redux.Gameplay.Widescreen) {
-        ChangeBytes -Offset "952BB"  -Values "E3 34 E7 8E 39"
-        ChangeBytes -Offset "953EF"  -Values "E3 34 E7 8E 39"
-        ChangeBytes -Offset "12322D" -Values "E3 8E 39 3F 63 8E 39 40 63 8E 39 3F E3 8E 39"
+        ChangeBytes -Offset "952BB"  -Values "E334E78E39"
+        ChangeBytes -Offset "953EF"  -Values "E334E78E39"
+        ChangeBytes -Offset "12322D" -Values "E38E393F638E3940638E393FE38E39"
     }
 
     if (IsChecked $Redux.Gameplay.SixtyFrames) {
-        ChangeBytes -Offset "2490" -Values "24 19 00 01 24 19 00 01"; ChangeBytes -Offset "1BE3" -Values "00"; ChangeBytes -Offset "20CF" -Values "01"
-        ChangeBytes -Offset "2890" -Values "24 0A 00 01 24 0A 00 01"; ChangeBytes -Offset "2638" -Values "24 09 00 01 24 09 00 01"
+        ChangeBytes -Offset "2490" -Values "2419000124190001"; ChangeBytes -Offset "1BE3" -Values "00"; ChangeBytes -Offset "20CF" -Values "01"
+        ChangeBytes -Offset "2890" -Values "240A0001240A0001"; ChangeBytes -Offset "2638" -Values "2409000124090001"
     }
 
     if (IsChecked $Redux.Gameplay.RemoveRubberBanding)   { ChangeBytes -Offset "297AA" -Values "B49A"                                         }
     if (IsChecked $Redux.Gameplay.MultiplayerMusic)      { ChangeBytes -Offset "F82BF" -Values "00"; ChangeBytes -Offset "F8FE7" -Values "00" }
-    if (IsChecked $Redux.Gameplay.NoCameraShake)         { ChangeBytes -Offset "8E018" -Values "2400" }
+    if (IsChecked $Redux.Gameplay.NoCameraShake)         { ChangeBytes -Offset "8E018" -Values "2400"                                         }
+    if (IsChecked $Redux.Gameplay.AlwaysAdvance)         { ChangeBytes -Offset "F8600" -Values "00000000"                                     }
 
 }
 
@@ -40,6 +41,7 @@ function CreateOptions() {
     CreateReduxCheckBox -Name "RemoveRubberBanding" -Text "Remove Rubber Banding" -Info "Removes the rubber banding from CPU players`nRubber banding causes CPUs to adjust their speed to that of the player's position" -Credits "Admentus (ROM)"
     CreateReduxCheckBox -Name "MultiplayerMusic"    -Text "Multiplayer Music"     -Info "Enable music for 3-Player and 4-Player Mode`nPress L to toggle the music"                                                       -Credits "Zoinkity"
     CreateReduxCheckBox -Name "NoCameraShake"       -Text "No Camera Shake"       -Info "The camera won't shake when using mushrooms"                                                                                    -Credits "Triclon"
+    CreateReduxCheckBox -Name "AlwaysAdvance"       -Text "Always Advance"        -Info "You always advance to the next race, even if you end up 5th place or lower"                                                     -Credits "Litronom"
 
     EnableElem -Elem $Redux.Gameplay.CPUItems -Active (!$IsWiiVC)
 
