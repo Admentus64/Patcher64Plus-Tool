b1a23ce0b4692a3e69ea03585d54945a94224edf
Shadow <shadow_one666@hotmail.com>
2022-08-17 15:59:51 -0600

    Update Zelda 64.psm1

--- Files/Scripts/Zelda 64.psm1
+++ Files/Scripts/Zelda 64.psm1
@@ -944,7 +944,7 @@ function CreateFairyColorOptions($name) {
 
     # FAIRY COLORS #
     CreateReduxGroup    -Tag  "Colors" -All -Text "Fairy Colors" -Height 2
-    $Items = @($GameType.default_values.fairy_option1, $GameType.default_values.fairy_option2, "Tael", "Gold", "Green", "Light Blue", "Yellow", "Red", "Magenta", "Black", "Fi", "Ciela", "Epona", "Ezlo", "King of Red Lions", "Linebeck", "Loftwing", "Midna", "Phantom Zelda", "Randomized", "Custom")
+    $Items = @($GameType.default_values.fairy_option1, $GameType.default_values.fairy_option2, "Tael", "Tatl (Cutscene)", "Tael (Cutscene)", "Gold", "Green", "Light Blue", "Yellow", "Red", "Magenta", "Black", "Fi", "Ciela", "Epona", "Ezlo", "King of Red Lions", "Linebeck", "Loftwing", "Midna", "Phantom Zelda", "Randomized", "Custom")
     $Presets = ("`n" + 'Selecting the presets ' + '"' + $GameType.default_values.fairy_option1 + '"' + ' or "Tael" will also change the references for ' + '"' + $GameType.default_values.fairy_option1 + '"' + ' in the dialogue')
     CreateReduxComboBox -Name "Fairy" -Length 230 -Shift 40 -Items $Items -All -Text ($name + " Colors") -Info ("Select a color scheme for " + $name + $Presets + "`n" + '"Randomized" fully randomizes the colors each time the patcher is opened') -Credits "Ported from Rando"
 
