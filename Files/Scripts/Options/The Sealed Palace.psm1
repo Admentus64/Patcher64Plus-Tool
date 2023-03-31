8526c5e2498c7a94b10953da76a2f6c19834a9bc
Admentus64 <robin.hallink@gmail.com>
2023-03-31 18:19:17 +0200

    Hotfix with more options

--- Files/Scripts/Options/The Sealed Palace.psm1
+++ Files/Scripts/Options/The Sealed Palace.psm1
@@ -4,7 +4,24 @@ function PatchOptions() {
 
     if (IsChecked $Redux.Graphics.MMChildLink)   { ApplyPatch -Patch "Decompressed\Optional\The Sealed Palace\tsp_mm_child_link.ppf" }
     if (IsChecked $Redux.Graphics.MMAdultLink)   { ApplyPatch -Patch "Decompressed\Optional\The Sealed Palace\tsp_mm_adult_link.ppf" }
-    
+
+}
+
+
+
+#==============================================================================================================================================================================================
+function ByteOptions() {
+
+    # GAMEPLAY #
+
+    if (IsChecked $Redux.Gameplay.ResumeLastArea) { ChangeBytes -Offset "B1F7AC" -Values "0000"; ChangeBytes -Offset "B1F7A0" -Values "0000" }
+
+
+
+    # HERO MODE #
+
+    if (IsChecked $Redux.HeroMode.NoHeartDrops) { ChangeBytes -Offset "A966C7"  -Values "2E" } 
+
 }
 
 
@@ -12,10 +29,27 @@ function PatchOptions() {
 #==============================================================================================================================================================================================
 function CreateOptions() {
     
-    CreateOptionsDialog -Columns 2 -Height 200
+    CreateOptionsDialog -Columns 2 -Height 300
     
-    # OTHER #
-    CreateReduxGroup    -All -Tag  "Graphics" -Text "Graphics" 
+
+
+    # GAMEPLAY #
+
+    CreateReduxGroup    -All -Tag  "Gameplay"       -Text "Gameplay"
+    CreateReduxCheckBox -All -Name "ResumeLastArea" -Text "Resume From Last Area" -Info "Resume playing from the area you last saved in" -Warning "Don't save in Grottos" -Credits "Aegiker"
+
+
+
+    # HERO MODE #
+
+    CreateReduxGroup    -All -Tag  "Hero"         -Text "Hero Mode"
+    CreateReduxCheckBox -All -Name "NoHeartDrops" -Text "No Heart Drops" -Info "Remove Recovery Heart drops from the game" -Credits "Admentus"
+
+
+
+    # GRAPHICS #
+
+    CreateReduxGroup    -All -Tag  "Graphics"    -Text "Graphics" 
     CreateReduxCheckBox -All -Name "MMChildLink" -Text "MM Child Link" -Info "Replaces the Child Link model with the one from Majora's Mask"         -Credits "Nintendo"
     CreateReduxCheckBox -All -Name "MMAdultLink" -Text "MM Adult Link" -Info "Replaces the Child Link model with the one styled after Majora's Mask" -Credits "Skilar"
 
