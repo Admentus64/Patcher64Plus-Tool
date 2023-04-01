e4a3f73a1d1297c7aa9de73848b6d62e2b204043
Admentus64 <robin.hallink@gmail.com>
2023-04-01 15:18:43 +0200

    Update v20.2.3

--- Files/Scripts/Options/The Sealed Palace.psm1
+++ Files/Scripts/Options/The Sealed Palace.psm1
@@ -2,9 +2,9 @@ function PatchOptions() {
     
     # MODELS #
 
-    if (IsChecked $Redux.Graphics.MMChildLink)   { ApplyPatch -Patch "Decompressed\Optional\The Sealed Palace\tsp_mm_child_link.ppf" }
     if (IsChecked $Redux.Graphics.MMAdultLink)   { ApplyPatch -Patch "Decompressed\Optional\The Sealed Palace\tsp_mm_adult_link.ppf" }
-
+    if (IsChecked $Redux.Graphics.MMChildLink)   { ApplyPatch -Patch "Decompressed\Optional\The Sealed Palace\tsp_mm_child_link.ppf" }
+    
 }
 
 
@@ -20,7 +20,21 @@ function ByteOptions() {
 
     # HERO MODE #
 
-    if (IsChecked $Redux.HeroMode.NoHeartDrops) { ChangeBytes -Offset "A966C7"  -Values "2E" } 
+    if (IsChecked $Redux.HeroMode.NoHeartDrops) { ChangeBytes -Offset "A966C7" -Values "2E" }
+
+
+
+    # ANIMATIONS #
+
+    if     (IsChecked $Redux.Animation.WeaponIdle)        { ChangeBytes -Offset "1B9AA5E" -Values "34 28"                                                                                                             }
+    if     (IsChecked $Redux.Animation.WeaponCrouch)      { ChangeBytes -Offset "1B9ACCE" -Values "2A 10"                                                                                                             }
+    if     (IsChecked $Redux.Animation.WeaponAttack)      { ChangeBytes -Offset "1B9B892" -Values "2B D8";               ChangeBytes -Offset "1B9B896" -Values "2B E0"; ChangeBytes -Offset "1B9B89A" -Values "2B E0" }
+    if     (IsChecked $Redux.Animation.HoldShieldOut)     { ChangeBytes -Offset "B9CE47"  -Values "02 0A 0A 10"                                                                                                       }
+    if     (IsChecked $Redux.Animation.BackflipAttack)    { ChangeBytes -Offset "1B9B842" -Values "29 D0"                                                                                                             }
+    elseif (IsChecked $Redux.Animation.FrontflipAttack)   { ChangeBytes -Offset "1B9B842" -Values "2A 60"                                                                                                             }
+    if     (IsChecked $Redux.Animation.FrontflipJump)     { PatchBytes  -Offset "69A700"  -Patch "Jumps\frontflip.bin"                                                                                                }
+    elseif (IsChecked $Redux.Animation.SomarsaultJump)    { PatchBytes  -Offset "69A700"  -Patch "Jumps\somarsault.bin"; ChangeBytes -Offset "1EF8149" -Values "0E"                                                   }
+    if     (IsChecked $Redux.Animation.SideBackflip)      { ChangeBytes -Offset "1B9BC4A" -Values "29 88"                                                                                                             }
 
 }
 
@@ -29,7 +43,7 @@ function ByteOptions() {
 #==============================================================================================================================================================================================
 function CreateOptions() {
     
-    CreateOptionsDialog -Columns 2 -Height 300
+    CreateOptionsDialog -Columns 4 -Height 410
     
 
 
@@ -52,5 +66,22 @@ function CreateOptions() {
     CreateReduxGroup    -All -Tag  "Graphics"    -Text "Graphics" 
     CreateReduxCheckBox -All -Name "MMChildLink" -Text "MM Child Link" -Info "Replaces the Child Link model with the one from Majora's Mask"         -Credits "Nintendo"
     CreateReduxCheckBox -All -Name "MMAdultLink" -Text "MM Adult Link" -Info "Replaces the Child Link model with the one styled after Majora's Mask" -Credits "Skilar"
+
+
+
+    # ANIMATIONS #
+
+    $weapons = "`n`nAffected weapons:`n- Giant's Knife`n- Giant's Knife (Broken)`n- Biggoron Sword`n- Deku Stick`n- Megaton Hammer"
+
+    CreateReduxGroup    -Tag  "Animation"       -All -Text "Link Animations"
+    CreateReduxCheckBox -Name "WeaponIdle"      -All -Text "2-handed Weapon Idle"   -Info ("Restore the beta animation when idly holding a two-handed weapon" + $weapons)                      -Credits "Admentus"
+    CreateReduxCheckBox -Name "WeaponCrouch"    -All -Text "2-handed Weapon Crouch" -Info ("Restore the beta animation when crouching with a two-handed weapon" + $weapons)                    -Credits "Admentus"
+    CreateReduxCheckBox -Name "WeaponAttack"    -All -Text "2-handed Weapon Attack" -Info ("Restore the beta animation when attacking with a two-handed weapon" + $weapons)                    -Credits "Admentus"
+    CreateReduxCheckBox -Name "HoldShieldOut"   -All -Text "Hold Shield Out"        -Info "Restore the beta animation for Link to always holds his shield out even when his sword is sheathed" -Credits "Admentus"
+    CreateReduxCheckBox -Name "BackflipAttack"  -All -Text "Backflip Jump Attack"   -Info "Restore the beta animation to turn the Jump Attack into a Backflip Jump Attack"                     -Credits "Admentus"
+    CreateReduxCheckBox -Name "FrontflipAttack" -All -Text "Frontflip Jump Attack"  -Info "Restore the beta animation to turn the Jump Attack into a Frontflip Jump Attack"                    -Credits "Admentus"    -Link $Redux.Animation.BackflipAttack
+    CreateReduxCheckBox -Name "FrontflipJump"   -All -Text "Frontflip Jump"         -Info "Replace the jumps with frontflip jumps`nThis is a jump from Majora's Mask"                          -Credits "SoulofDeity"
+    CreateReduxCheckBox -Name "SomarsaultJump"  -All -Text "Somarsault Jump"        -Info "Replace the jumps with somarsault jumps`nThis is a jump from Majora's Mask"                         -Credits "SoulofDeity" -Link $Redux.Animation.FrontflipJump
+    CreateReduxCheckBox -Name "SideBackflip"    -All -Text "Side Backflip"          -Info "Replace the backflip jump with side hop jumps"                                                      -Credits "BilonFullHDemon"
 
 }
