a465b9eec2898e894559a65bd70eb16a52a764f7
Admentus64 <robin.hallink@gmail.com>
2023-04-23 16:01:50 +0200

    Hotfix 1
    - Remove Redux VC title headers
    - Only show hotfix info if there is

--- Files/Scripts/Main.psm1
+++ Files/Scripts/Main.psm1
@@ -103,8 +103,12 @@ function CreateMainDialog() {
     $CurrentModeLabel.AutoSize = $True
 
     # Create a label to show current version.
-    $VersionLabel = CreateLabel -X (DPISize 15) -Y (DPISize 30) -Width (DPISize 150) -Height (DPISize 30) -Text ($Patcher.Version + " - #" + $Patcher.Hotfix + "`n(" + $Patcher.Date + ")") -Font $Fonts.SmallBold -AddTo $MainDialog
-    $osLabel      = CreateLabel -X (DPISize 15) -Y (DPISize 60) -Width (DPISize 250) -Height (DPISize 20) -Text ($Patcher.OS + " (" + $Patcher.Bit + ")")        -Font $Fonts.SmallBold -AddTo $MainDialog
+    $text = $Patcher.Version
+    if (IsSet $Patcher.Hotfix) { $text += " - #" + $Patcher.Hotfix }
+    $text += "`n(" + $Patcher.Date + ")"
+
+    $VersionLabel = CreateLabel -X (DPISize 15) -Y (DPISize 30) -Width (DPISize 150) -Height (DPISize 30) -Text $text                                     -Font $Fonts.SmallBold -AddTo $MainDialog
+    $osLabel      = CreateLabel -X (DPISize 15) -Y (DPISize 60) -Width (DPISize 250) -Height (DPISize 20) -Text ($Patcher.OS + " (" + $Patcher.Bit + ")") -Font $Fonts.SmallBold -AddTo $MainDialog
     if (!([Environment]::Is64BitOperatingSystem)) { $osLabel.text += " NOT SUPPORTED"; $osLabel.ForeColor = "red" }
 
     # Create Arrays for groups
