0515d62cb6edbb5e208e54c572d0057e993d7aa7
Maria Nova <MariaGoldenSpaceNova777@gmail.com>
2024-03-12 18:01:41 -0400

    Revamped Majora's Mask GC Text
    The GC text has been completely revamped and revisited.

--- Files/Scripts/Options/Majora's Mask.psm1
+++ Files/Scripts/Options/Majora's Mask.psm1
@@ -1256,13 +1256,24 @@ function ByteTextOptions() {
     }
 
     if (IsChecked $Redux.UI.GCScheme -Lang 1) { 
-        SetMessage -ID "0227" -Text "5A20"  -Replace "4C20";      SetMessage -ID "1912"; SetMessage -ID "191D"; SetMessage -ID "1946"; SetMessage -ID "1954"                                                                                                      # Z -> L	  
-        SetMessage -ID "0014" -Text "press" -Replace "use";       SetMessage -ID "0015"; SetMessage -ID "0016"; SetMessage -ID "0017"; SetMessage -ID "0018"; SetMessage -ID "0019"; SetMessage -ID "0035"; SetMessage -ID "09E3"                                 # press -> use
-        SetMessage -ID "0078" -Text "Press <C><C Button><N><W>" -Replace "Use <C><C Button><N><W>"                                                                                                                                                                # N64 -> GC Text phrasing adjustments
-        SetMessage -ID "004C" -Text "<W> Buttons" -Replace "<W>"; SetMessage -ID "0059" -Text ", then press that <N><C><C Button> <W>Button to use it." -Replace "<W>."; SetMessage -ID "1700" -Text "<C Button><N>Buttons. Press" -Replace "<C Button>.<N>Press" # Buttons -> C Icon Only	    
-        
-        SetMessage -ID "001A" -Text "Press" -Replace "Use"; SetMessage -ID "0022" -All; SetMessage -ID "0028"; SetMessage -ID "0029"; SetMessage -ID "002A"; SetMessage -ID "002E"; SetMessage -ID "0034"; SetMessage -ID "0036"; SetMessage -ID "003A"; SetMessage -ID "0041"; SetMessage -ID "0043"; SetMessage -ID "1701" # Press -> Use
-        SetMessage -ID "1706";                              SetMessage -ID "1709";      SetMessage -ID "170E"; SetMessage -ID "170F"; SetMessage -ID "1717"; SetMessage -ID "171B"; SetMessage -ID "172E"; SetMessage -ID "0079"; SetMessage -ID "007A"
+	SetMessage -ID "004C" -Text "four <C><C Button><W> Buttons to play it. Press<N><B Button> to stop." -Replace "four <C><C Button><W> directions to play it.<N>Press <B Button> to stop."
+        SetMessage -ID "1700" -Text "<C Button><N>Buttons. Press" -Replace "<C Button>.<N>Press"
+	
+        SetMessage -ID "001A" -Text "Press" -Replace "Use";      SetMessage -ID "0022" -All; SetMessage -ID "0028"; SetMessage -ID "0029"; SetMessage -ID "002A"; SetMessage -ID "002E"; SetMessage -ID "0034"; SetMessage -ID "0036"
+        SetMessage -ID "003A"; SetMessage -ID "0041"; SetMessage -ID "0043"; SetMessage -ID "0078"; SetMessage -ID "1701"; SetMessage -ID "1706"; SetMessage -ID "1709"; SetMessage -ID "170E"; SetMessage -ID "170F"; SetMessage -ID "1717"
+	SetMessage -ID "171B"; SetMessage -ID "172E"
+
+        SetMessage -ID "0014" -Text "press" -Replace "use"; SetMessage -ID "0015"; SetMessage -ID "0016"; SetMessage -ID "0017"; SetMessage -ID "0018"; SetMessage -ID "0019"; SetMessage -ID "0035"; SetMessage -ID "0200"; SetMessage -ID "09E3"
+	SetMessage -ID "1F4F"
+
+        SetMessage -ID "0078"; -Text "Deku. Press" -Replace "Deku. Use"
+	SetMessage -ID "0079"; -Text "Goron. Press" -Replace "Goron. Use"
+        SetMessage -ID "007A"; -Text "Zora. Press" -Replace "Zora. Use"
+
+        SetMessage -ID "0043" -Text "Press <C><C Button> <W>to look through it" -Replace "Use <C><C Button> <W>to look through it"
+	SetMessage -ID "0059" -Text "then press that <N><C><C Button> <W>Button to use it." -Replace "then use <N><C><C Button><W>."
+
+        if (IsChecked $Redux.Features.OcarinaIcons) { SetMessage -ID "1726" -Text "<C Button><N>Buttons. Press" -Replace "<C Button>.<N>Press"; SetMessage -ID "171C"; SetMessage -ID "1727" }
     }
 
     if (IsChecked $Redux.Equipment.PermanentRazorSword -Lang 1) {
